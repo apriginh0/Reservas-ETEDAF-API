7f75af85738e4d8a696c722f4396e4ff21994582
apriginh0 <apriginh0@hotmail.com>
2025-02-11 21:59:03 -0300

    Atualizacao do CORS

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL; // Ajuste para seu frontend local
 const allowedOrigins = [
-  "http://localhost:8100",
-  "http://localhost",       // Para chamadas diretas
   FRONTEND_URL,             // Seu frontend na Vercel (ex: "https://www.etedaf.com.br")
   null                      // Permitir chamadas de apps móveis
 ];
