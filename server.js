0139589a752afd3a6803c85538237ba057503606
apriginh0 <apriginh0@hotmail.com>
2025-02-27 21:14:16 -0300

    Atualizando login

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    exposedHeaders: ['set-cookie', 'clear-site-data'] // Explicita headers de cookies
+    exposedHeaders: ['set-cookie'] // Explicita headers de cookies
   })
 );
 
